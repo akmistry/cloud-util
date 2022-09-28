5018947d2321a695c9a1c21b45396696b0da20bf
Anand K. Mistry <anand@mistry.ninja>
2022-09-28 21:19:49 +1000

    Add S3 backend backed on GoCloud

---
+++ aws/gocloud_s3.go
@@ -0,0 +1,240 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	//"net/http"
+	"os"
+	//"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"gocloud.dev/blob"
+	"gocloud.dev/blob/s3blob"
+	"gocloud.dev/gcerrors"
+	"golang.org/x/sync/semaphore"
+
+	"github.com/akmistry/cloud-util"
+)
+
+//var flagS3DisableTls = flag.Bool("s3-disable-tls", false, "Disable TLS for S3")
+
+//const maxPendingFetches = 512
+
+type GCS3Store struct {
+	session     *session.Session
+	bucket      *blob.Bucket
+	pendingSema *semaphore.Weighted
+}
+
+func NewDefaultGCS3Store(bucket string) *GCS3Store {
+	return NewGCS3Store(*Endpoint, *AccessId, *AccessSecret, *Region, bucket)
+}
+
+func NewGCS3Store(endpoint, accessKey, secret, region, bucket string) *GCS3Store {
+	/*
+		transport := &http.Transport{
+			MaxIdleConnsPerHost:   1024,
+			IdleConnTimeout:       90 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			DisableCompression:    true,
+		}
+	*/
+
+	session, err := session.NewSession(&aws.Config{
+		Endpoint:         aws.String(endpoint),
+		Credentials:      credentials.NewStaticCredentials(accessKey, secret, ""),
+		Region:           aws.String(region),
+		DisableSSL:       aws.Bool(*flagS3DisableTls),
+		S3ForcePathStyle: aws.Bool(true),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a *blob.Bucket.
+	blobBucket, err := s3blob.OpenBucket(context.TODO(), session, bucket, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return &GCS3Store{
+		session:     session,
+		bucket:      blobBucket,
+		pendingSema: semaphore.NewWeighted(maxPendingFetches),
+	}
+}
+
+func (s *GCS3Store) Size(name string) (int64, error) {
+	s.pendingSema.Acquire(context.Background(), 1)
+	defer s.pendingSema.Release(1)
+
+	attr, err := s.bucket.Attributes(context.TODO(), name)
+	if gcerrors.Code(err) == gcerrors.NotFound {
+		return 0, os.ErrNotExist
+	} else if err != nil {
+		return 0, err
+	}
+
+	return attr.Size, nil
+}
+
+type goCloudGetReader struct {
+	s    *GCS3Store
+	name string
+	size int64
+}
+
+/*
+func createRange(off, len int64) string {
+	return fmt.Sprintf("bytes=%d-%d", off, off+len-1)
+}
+*/
+
+func (r *goCloudGetReader) Size() int64 {
+	return r.size
+}
+
+func (r *goCloudGetReader) ReadAt(b []byte, off int64) (int, error) {
+	if off >= r.size {
+		return 0, io.EOF
+	}
+	oldLen := len(b)
+	if int64(len(b)) > (r.size - off) {
+		b = b[:int(r.size-off)]
+	}
+
+	r.s.pendingSema.Acquire(context.Background(), 1)
+	defer r.s.pendingSema.Release(1)
+
+	rr, err := r.s.bucket.NewRangeReader(context.TODO(), r.name, off, int64(len(b)), nil)
+	if gcerrors.Code(err) == gcerrors.NotFound {
+		return 0, os.ErrNotExist
+	} else if err != nil {
+		return 0, err
+	}
+	defer rr.Close()
+	n, err := io.ReadFull(rr, b)
+	if err == nil && n < oldLen {
+		err = io.EOF
+	}
+	return n, err
+}
+
+func (r *goCloudGetReader) Close() error {
+	return nil
+}
+
+func (s *GCS3Store) Get(name string) (cloud.GetReader, error) {
+	size, err := s.Size(name)
+	if err != nil {
+		return nil, err
+	}
+	r := &goCloudGetReader{s: s, name: name, size: size}
+	return r, nil
+}
+
+type goCloudPutWriter struct {
+	s      *GCS3Store
+	pw     *io.PipeWriter
+	result chan result
+	closed bool
+}
+
+func (w *goCloudPutWriter) Write(b []byte) (int, error) {
+	n, err := w.pw.Write(b)
+	if err != nil {
+		err = fmt.Errorf("error writing %d bytes: %v", len(b), err)
+	}
+	return n, err
+}
+
+func (w *goCloudPutWriter) Close() error {
+	if w.closed {
+		return nil
+	}
+	w.closed = true
+
+	w.pw.Close()
+
+	res := <-w.result
+	if res.err != nil {
+		return res.err
+	}
+	return nil
+}
+
+func (w *goCloudPutWriter) Cancel() error {
+	if w.closed {
+		return nil
+	}
+	w.closed = true
+
+	// TODO: ErrCanceled?
+	w.pw.CloseWithError(io.ErrClosedPipe)
+	w.pw = nil
+
+	// TODO: Wait for request completion, and delete the object if success.
+	return nil
+}
+
+/*
+type result struct {
+	n   int64
+	err error
+}
+*/
+
+func (s *GCS3Store) Put(name string) (cloud.PutWriter, error) {
+	s.pendingSema.Acquire(context.Background(), 1)
+	r, pw := io.Pipe()
+	writer := &goCloudPutWriter{s: s, pw: pw, result: make(chan result, 1)}
+	go func() {
+		defer r.Close()
+		defer close(writer.result)
+		defer s.pendingSema.Release(1)
+
+		ctx, cf := context.WithCancel(context.TODO())
+		defer cf()
+
+		w, err := s.bucket.NewWriter(ctx, name, nil)
+		var n int64
+		if err == nil {
+			n, err = io.Copy(w, r)
+			if err == nil {
+				w.Close()
+			} else {
+				cf()
+				w.Close()
+			}
+		}
+		if err != nil {
+			log.Print("Error putting object: ", err)
+		}
+		writer.result <- result{n: n, err: err}
+	}()
+	return writer, nil
+}
+
+func (s *GCS3Store) Delete(name string) error {
+	return s.bucket.Delete(context.TODO(), name)
+}
+
+func (s *GCS3Store) List() ([]string, error) {
+	names := make([]string, 0)
+
+	iter := s.bucket.List(nil)
+	for {
+		obj, err := iter.Next(context.TODO())
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		names = append(names, obj.Key)
+	}
+	return names, nil
+}
--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.19
 require (
 	cloud.google.com/go/storage v1.26.0
 	github.com/akmistry/go-util v0.0.0-20220826051432-1e3e533181fe
+	github.com/aws/aws-sdk-go v1.43.31
 	github.com/docker/libkv v0.2.1
+	github.com/hashicorp/golang-lru v0.5.1
 	github.com/minio/minio-go/v7 v7.0.37
 	github.com/prometheus/client_golang v1.13.0
+	gocloud.dev v0.26.0
 	golang.org/x/sync v0.0.0-20220907140024-f12130a52804
 )
 
@@ -16,18 +19,37 @@ require (
 	cloud.google.com/go/compute v1.7.0 // indirect
 	cloud.google.com/go/iam v0.3.0 // indirect
 	github.com/AndreasBriese/bbloom v0.0.0-20190825152654-46b345b51c96 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.16.2 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.15.3 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.11.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.26.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.3 // indirect
+	github.com/aws/smithy-go v1.11.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgraph-io/badger v1.6.2 // indirect
 	github.com/dgraph-io/ristretto v0.0.2 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/wire v0.5.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
